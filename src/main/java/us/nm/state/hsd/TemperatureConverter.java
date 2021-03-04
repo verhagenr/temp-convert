23a71fff07a32a470b9ea01da603aabdba1b1790
Verhagenr <Verhagenr@ITDSFI-G022423D.nmhsd.lcl>
2021-03-04 16:03:50 -0700

    Completed

--- src/main/java/us/nm/state/hsd/TemperatureConverter.java
+++ src/main/java/us/nm/state/hsd/TemperatureConverter.java
@@ -3,29 +3,26 @@ package us.nm.state.hsd;
 public class TemperatureConverter {
 
     public static double celsiusToFahrenheit(double celsius) {
-
-	return celsius * 9 / 5 + 32;
+	return (celsius * 9 / 5) + 32;
     }
 
     public static double celsiusToKelvin(double celsius) {
 	return celsius + 273.15;
-	
     }
 
     public static double kelvinToFahrenheit(double kelvin) {
-	throw new UnsupportedOperationException("Not yet implemented"); // TODO Complete implementation
+	return (kelvin - 273.15) * 9 / 5 + 32;
     }
 
     public static double kelvinToCelsius(double kelvin) {
-	throw new UnsupportedOperationException("Not yet implemented"); // TODO Complete implementation
+	return kelvin - 273.15;
     }
 
     public static double fahrenheitToCelsius(double fahrenheit) {
 	return (fahrenheit - 32) * 5 / 9;
-    
     }
 
     public static double fahrenheitToKelvin(double fahrenheit) {
-	throw new UnsupportedOperationException("Not yet implemented"); // TODO Complete implementation
+	return (fahrenheit - 32) * 5 / 9 + 273.15;
     }
 }
--- src/test/java/us/nm/state/hsd/TemperatureConverterTest.java
+++ src/test/java/us/nm/state/hsd/TemperatureConverterTest.java
@@ -2,32 +2,50 @@ package us.nm.state.hsd;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class TemperatureConverterTest {
 
     static final double FAHRENHEIT_TOLERANCE = 0.001;
-    static double KELVIN_TOLERANCE = 0.0005556;
-    
+    static final double KELVIN_TOLERANCE = 0.0005556;
+    static final double CELSIUS_TOLERANCE = 0.0005556;
+
     @ParameterizedTest
     @CsvFileSource(resources = "temperatures.csv", numLinesToSkip = 1)
-    void celciusToFahrenheit(double celcius, double fahrenheit) {
-	assertEquals(fahrenheit,
-		     TemperatureConverter.celsiusToFahrenheit(celcius),
-		     FAHRENHEIT_TOLERANCE
-	);
+    void celsiusToFahrenheit(double celsius, double fahrenheit) {
+	assertEquals(fahrenheit, TemperatureConverter.celsiusToFahrenheit(celsius), FAHRENHEIT_TOLERANCE);
     }
 
     @ParameterizedTest
     @CsvFileSource(resources = "temperatures.csv", numLinesToSkip = 1)
-    void celciusToKelvin(double celcius, double fahrenheit, double kelvin) {
-	assertEquals(
-		     kelvin,
-		     TemperatureConverter.celsiusToKelvin(celcius),
-		     KELVIN_TOLERANCE
-	);
+    void celsiusToKelvin(double celsius, double fahrenheit, double kelvin) {
+	assertEquals(kelvin, TemperatureConverter.celsiusToKelvin(celsius), KELVIN_TOLERANCE);
     }
 
+    @ParameterizedTest
+    @CsvFileSource(resources = "temperatures.csv", numLinesToSkip = 1)
+    void kelvinToFahrenheit(double celsius, double fahrenheit, double kelvin) {
+	assertEquals(fahrenheit, TemperatureConverter.kelvinToFahrenheit(kelvin), FAHRENHEIT_TOLERANCE);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "temperatures.csv", numLinesToSkip = 1)
+    void kelvinToCelsius(double celsius, double fahrenheit, double kelvin) {
+
+	assertEquals(celsius, TemperatureConverter.kelvinToCelsius(kelvin), CELSIUS_TOLERANCE);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "temperatures.csv", numLinesToSkip = 1)
+    void fahrenheitToCelsius(double celsius, double fahrenheit) {
+
+	assertEquals(celsius, TemperatureConverter.fahrenheitToCelsius(fahrenheit), CELSIUS_TOLERANCE);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "temperatures.csv", numLinesToSkip = 1)
+    void fahrenheitToKelvin(double celsius, double fahrenheit, double kelvin) {
+	assertEquals(kelvin, TemperatureConverter.fahrenheitToKelvin(fahrenheit), KELVIN_TOLERANCE);
+    }
 }
